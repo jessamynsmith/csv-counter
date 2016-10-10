a16105237d12f59a481df794914bb23c5431f40c
Jessamyn Smith <geekchick77@gmail.com>
2016-10-10 00:38:07 -0400

    Explore a CSV file: For every field in the file, count up occurrences of unique values, and display the 5 most common.

---
+++ README.md
@@ -0,0 +1,21 @@
+csv-counter
+===========
+
+Unicode CSV reader example. Reads in data and keeps counters.
+
+Development
+-----------
+
+Fork the project on github and git clone your fork, e.g.:
+
+    git clone https://github.com/<username>/csv-counter.git
+
+Create a virtualenv using Python 3 and install dependencies. I recommend getting python3 via homebrew as well, then installing [virtualenv](https://virtualenv.pypa.io/en/latest/installation.html) and [virtualenvwrapper](https://virtualenvwrapper.readthedocs.org/en/latest/install.html#basic-installation) to that python. NOTE! You must change 'path/to/python3'
+to be the actual path to python3 on your system.
+
+    mkvirtualenv eggtimer --python=/path/to/python3
+    pip install -r requirements.txt
+    
+Run script:
+
+    python csv_counter.py
---
+++ csv_counter.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# *Goal:*
+# Analyze the data in a CSV file.
+
+# *Task:*
+# Given a CSV with arbitrary fields:
+#   - Find and show 5 most common values in each field, and their counts
+
+
+import unicodecsv as csv
+from collections import Counter
+
+
+def count_data(infile):
+    # Dictionary of counters (will have one for each field in the CSV
+    counters = {}
+    # E.g.
+    # counters = {
+    #     Keep track of every occurring value in the row
+    #     'Sector': Counter({
+    #           'Municipality': 10805,
+    #           'School Board': 5010,
+    #           'Post-Secondary Institution', 756
+    #     }),
+    #     'City': Counter({
+    #           'Toronto': 1676,
+    #           'Ottawa': 539,
+    #           'Mississauga', 384
+    #     }),
+    # }
+
+    with open(infile, "rb") as csv_in:
+        reader = csv.DictReader(csv_in, encoding='utf-8')
+
+        # Initialize counters, creating one for each field
+        for fieldname in reader.fieldnames:
+            counters[fieldname] = Counter()
+
+        # Iterate over lines in CSV
+        for row in reader:
+            # Iterate over the fields in the row
+            for fieldname in row.keys():
+                field_value = row[fieldname]
+                # Update the counter for this field, incrementing the count for the value from this row
+                counters[fieldname][field_value] += 1
+
+    return counters
+
+
+# If the script is being invoked as main, run the counter functionality
+if __name__ == "__main__":
+
+    # Input CSV file
+    infile = 'data/bps_raw_data_2013_revised_en.csv'
+    counters = count_data(infile)
+
+    # For every counter, print out the top 3
+    for fieldname in sorted(counters.keys()):
+        print('%s: %s' % (fieldname, counters[fieldname].most_common(5)))
